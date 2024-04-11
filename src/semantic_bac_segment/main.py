ca23d7bf0bf31452f4c64c5bf263b8cc8704ab62
tisalon <tisalon@outlook.com>
2024-04-11 09:18:14 +0200

    add if __name__ == 'main'

--- src/semantic_bac_segment/main.py
+++ src/semantic_bac_segment/main.py
@@ -9,16 +9,20 @@ from semantic_bac_segment.data_loader import collate_fn
 from semantic_bac_segment.utils import read_cofig
 
 
-# Get config settings
-train_settings=read_cofig('./train_config.yaml')
+
+if __name__ == '__main__':
+    
+    train_settings = read_cofig('./train_config.yaml')
+    # Get config settings
+    train_settings=read_cofig('./train_config.yaml')
 
 
-# Init trainer with data and model
-trainer = UNetTrainer(**train_settings['trainer_params'])
-trainer.add_model(UNet2, **train_settings['model_params'])
-trainer.read_data(**train_settings['data_params'], collate_fn=collate_fn)
+    # Init trainer with data and model
+    trainer = UNetTrainer(**train_settings['trainer_params'])
+    trainer.add_model(UNet2, **train_settings['model_params'])
+    trainer.read_data(**train_settings['data_params'], collate_fn=collate_fn)
 
-# Train the model
-#criterion=DiceLoss(is_sigmoid=False)
-criterion=WeightedBinaryCrossEntropy(is_sigmoid=False)
-trainer.train(**train_settings['optimizer_params'], criterion=criterion)
+    # Train the model
+    #criterion=DiceLoss(is_sigmoid=False)
+    criterion=WeightedBinaryCrossEntropy(is_sigmoid=False)
+    trainer.train(**train_settings['optimizer_params'], criterion=criterion)
