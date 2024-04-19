7632aa6396d244af5cbc57e6abae3653a0285f73
tisalon <tisalon@outlook.com>
2024-04-19 11:20:15 +0200

    add unet variant, attention

---
+++ src/semantic_bac_segment/models/pytorch_attention.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as TF
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class AttentionBlock(nn.Module):
+    def __init__(self, F_g, F_l, F_int):
+        super(AttentionBlock, self).__init__()
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        return x * psi
+
+class AttentionUNet(nn.Module):
+    def __init__(
+            self, in_channels=1, out_channels=1, features=[64, 128, 256, 512], init_features=64, pooling_steps=4
+    ):
+        super(AttentionUNet, self).__init__()
+
+        if features == None:        
+            features = [2**i for i in range(pooling_steps) if 2**i >= init_features]
+        else:
+            features = features
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.attentions = nn.ModuleList()
+
+        # Down part of UNET
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        # Up part of UNET
+        for feature in reversed(features):
+            self.ups.append(
+                nn.ConvTranspose2d(
+                    feature*2, feature, kernel_size=2, stride=2,
+                )
+            )
+            self.attentions.append(AttentionBlock(feature, feature, feature // 2))
+            self.ups.append(DoubleConv(feature*2, feature))
+
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    @classmethod
+    def from_pretrained(cls, weights_path, **kwargs):
+        """
+        Create a UNet model and initialize it with pretrained weights.
+
+        Args:
+            weights_path (str): Path to the pretrained weights file.
+            **kwargs: Additional arguments to pass to the constructor.
+
+        Returns:
+            UNet: Initialized UNet model with pretrained weights.
+        """
+        model = cls(**kwargs)
+        model.load_weights(weights_path)
+        return model
+
+    def load_weights(self, weights_path):
+        """
+        Load weights from a previously trained model.
+
+        Args:
+            weights_path (str): Path to the weights file.
+        """
+        self.load_state_dict(torch.load(weights_path))
+
+            
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx//2]
+
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])
+
+            attention = self.attentions[idx//2](g=x, x=skip_connection)
+            concat_skip = torch.cat((attention, x), dim=1)
+            x = self.ups[idx+1](concat_skip)
+
+        return torch.sigmoid(self.final_conv(x))
