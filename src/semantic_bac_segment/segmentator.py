1f7acd007544e18bf455dbf5141d66d5b38675b7
tisalon <tisalon@outlook.com>
2024-04-19 11:20:35 +0200

    add segmentator3 from the other project

--- src/semantic_bac_segment/segmentator.py
+++ src/semantic_bac_segment/segmentator.py
@@ -230,3 +230,137 @@ class Segmentator2:
         
         except Exception as e:
             raise Exception(f"Unexpected error occurred: {str(e)}")
+        
+
+
+class Segmentator3:
+    """
+    A class representing a segmentation model.
+
+    Attributes:
+        model (torch.nn.Module): The segmentation model.
+        device (torch.device): The device on which the model is loaded.
+    """
+
+    def __init__(self, model_path, model_graph, patch_size, overlap_ratio, half_precision=False):
+        """
+        Initializes a Segmentator object.
+
+        Args:
+            model_path (str): The path to the model.
+            model_graph (str): The model graph.
+            patch_size (int): The size of the patches.
+            overlap_ratio (float): The overlap ratio between patches.
+        """
+        self.device = get_device()
+        self.model = self.get_model(model_path, self.device, model_graph=model_graph)
+        self.patch_size = patch_size
+        self.overlap_ratio = overlap_ratio
+        self.model.eval()
+        self.half_precision=half_precision
+        if self.half_precision:
+            self.model.half()  
+
+
+    def predict(self, image):
+        """
+        Predicts the segmentation mask for the given image. It can handle 2D images or a stack of 2D images.
+
+        Args:
+            image (numpy.ndarray): The input image or image stack.
+
+        Returns:
+            numpy.ndarray: The segmentation mask or stack of segmentation masks.
+        """
+        
+        # Normalize image
+        image = normalize_percentile(image)
+
+        # Check if the image is a stack
+        if len(image.shape) > 2:
+            # Store the original image size and number of slices
+            original_size = image.shape[1:]
+            num_slices = image.shape[0]
+
+            # Convert the entire stack to a tensor and add batch dimension
+            img_tensor = torch.from_numpy(image).unsqueeze(1).to(self.device)  # Shape: [1, C, H, W]
+            if self.half_precision:
+                img_tensor = img_tensor.half()  # Convert input to half-precision
+
+            # Create SlidingWindowInferer
+            inferer = SlidingWindowInferer(roi_size=self.patch_size, sw_batch_size=1, overlap=self.overlap_ratio)
+
+            with torch.no_grad():
+                output_mask = inferer(img_tensor, self.model)
+
+            output_mask = output_mask.squeeze(0).cpu().numpy()  # Remove batch dimension
+
+            # Split the result back into the original chunks
+            #output_mask = np.dsplit(output_mask, num_slices)
+
+        else:
+            # Process the image as before
+            img_tensor = torch.from_numpy(image).unsqueeze(0).unsqueeze(0).to(self.device)
+
+            if self.half_precision:
+                img_tensor = img_tensor.half()  # Convert input to half-precision
+
+            # Create SlidingWindowInferer
+            inferer = SlidingWindowInferer(roi_size=self.patch_size, sw_batch_size=350, overlap=self.overlap_ratio)
+
+            with torch.no_grad():
+                output_mask = inferer(img_tensor, self.model)
+
+            output_mask = output_mask.squeeze(0).squeeze(0).cpu().numpy()
+
+            # Free up tensors
+            del img_tensor, image  
+            gc.collect() 
+            empty_gpu_cache(self.device)
+
+        return output_mask
+   
+
+    def get_model(self, path, device, model_graph=None):
+        """
+        Loads a model from the specified path and returns it.
+
+        Args:
+            path (str): The path to the model file.
+            device (str): The device to load the model onto.
+            model_graph (Optional[torch.nn.Module]): An optional pre-initialized model graph.
+
+        Returns:
+            torch.nn.Module: The loaded model.
+
+        Raises:
+            FileNotFoundError: If the model file is not found at the specified path.
+            RuntimeError: If an error occurs while loading the model.
+            Exception: If an unexpected error occurs.
+        """
+        try:
+            if model_graph is None:
+                model = torch.load(path, map_location=device)
+            else:
+                model = model_graph
+                state_dict = torch.load(path, map_location=device)
+                
+                # Check if the loaded state dictionary is compatible with the model architecture
+                if not set(state_dict.keys()).issubset(set(model.state_dict().keys())):
+                    raise ValueError("Loaded state dictionary does not match the model architecture.")
+                
+                model.load_state_dict(state_dict)
+            
+            model.to(device)
+            torch.compile(model, mode = 'max-autotune')
+            
+            return model
+        
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Model file not found at path: {path}")
+        
+        except RuntimeError as e:
+            raise RuntimeError(f"Error occurred while loading the model: {str(e)}")
+        
+        except Exception as e:
+            raise Exception(f"Unexpected error occurred: {str(e)}")
