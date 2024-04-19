c261bf140ae7535c3c0fb0af5a08a828f460d638
tisalon <tisalon@outlook.com>
2024-04-19 11:20:05 +0200

    fix error with sigmoid at final_conv

--- src/semantic_bac_segment/models/pytorch_altmodel.py
+++ src/semantic_bac_segment/models/pytorch_altmodel.py
@@ -133,6 +133,5 @@ class UNET(nn.Module):
             concat_skip = torch.cat((skip_connection, x), dim=1)
             x = self.ups[idx+1](concat_skip)
         
-        x = torch.sigmoid(x)
 
-        return self.final_conv(x)
+        return torch.sigmoid(self.final_conv(x)) 
